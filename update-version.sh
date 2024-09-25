18b55344795597fb4cff7288ab275ee80cc5644e
Kiba Amor <5093911+kibaamor@users.noreply.github.com>
2024-09-25 18:36:33 +0800

    add gping and doggo

--- update-version.sh
+++ update-version.sh
@@ -5,72 +5,74 @@ cd "$(dirname -- "${BASH_SOURCE[0]}")" || exit
 filename="./home/.chezmoidata/versions.yaml"
 
 repos=(
-    "romkatv/powerlevel10k"
-    "zsh-users/zsh-autosuggestions"
-    "zsh-users/zsh-syntax-highlighting"
-    "MichaelAquilina/zsh-you-should-use"
+  "romkatv/powerlevel10k"
+  "zsh-users/zsh-autosuggestions"
+  "zsh-users/zsh-syntax-highlighting"
+  "MichaelAquilina/zsh-you-should-use"
 
-    "junegunn/vim-plug"
+  "junegunn/vim-plug"
 
-    "dandavison/delta"
-    "sharkdp/bat"
-    "sharkdp/fd"
-    "junegunn/fzf"
-    "direnv/direnv"
-    "BurntSushi/ripgrep"
-    "lsd-rs/lsd"
-    "muesli/duf"
-    "FiloSottile/age"
-    "tldr-pages/tlrc"
-    "fatedier/frp"
-    "bcicen/ctop"
-    "kubecolor/kubecolor"
-    "ahmetb/kubectx"
-    "wagoodman/dive"
-    "jesseduffield/lazydocker"
-    "jqlang/jq"
-    "mikefarah/yq"
-    "dundee/gdu"
-    "zu1k/nali"
-    "alexellis/arkade"
-    "FiloSottile/mkcert"
-    "kubernetes-sigs/kind"
-    "kevwan/tproxy"
+  "dandavison/delta"
+  "sharkdp/bat"
+  "sharkdp/fd"
+  "junegunn/fzf"
+  "direnv/direnv"
+  "BurntSushi/ripgrep"
+  "lsd-rs/lsd"
+  "muesli/duf"
+  "FiloSottile/age"
+  "tldr-pages/tlrc"
+  "fatedier/frp"
+  "bcicen/ctop"
+  "kubecolor/kubecolor"
+  "ahmetb/kubectx"
+  "wagoodman/dive"
+  "jesseduffield/lazydocker"
+  "jqlang/jq"
+  "mikefarah/yq"
+  "dundee/gdu"
+  "zu1k/nali"
+  "alexellis/arkade"
+  "FiloSottile/mkcert"
+  "kubernetes-sigs/kind"
+  "kevwan/tproxy"
+  "orf/gping"
+  "mr-karan/doggo"
 
-    "kubernetes/minikube"
-    "derailed/k9s"
-    "kubeshark/kubeshark"
-    "helm/helm"
-    "txn2/kubefwd"
+  "kubernetes/minikube"
+  "derailed/k9s"
+  "kubeshark/kubeshark"
+  "helm/helm"
+  "txn2/kubefwd"
 )
 
 echo "versions:" >$filename
 
 for repo in "${repos[@]}"; do
-    echo "processing repo: $repo"
+  echo "processing repo: $repo"
 
-    echo "  # https://github.com/$repo" >>$filename
+  echo "  # https://github.com/$repo" >>$filename
 
-    name=$(echo "$repo" | cut -d '/' -f 2 | tr -d '-')
-    #version=$(curl -s -L https://api.github.com/repos/$repo/releases/latest | jq '.tag_name' -r | tr -d '[a-z][A-Z] -')
+  name=$(echo "$repo" | cut -d '/' -f 2 | tr -d '-')
+  #version=$(curl -s -L https://api.github.com/repos/$repo/releases/latest | jq '.tag_name' -r | tr -d '[a-z][A-Z] -')
+  version=$(
+    gh api \
+      --method GET \
+      --header 'Accept: application/vnd.github+json' \
+      --jq '.tag_name' \
+      "https://api.github.com/repos/$repo/releases/latest" 2>/dev/null | grep -v 'Not Found' | tr -d '[a-z][A-Z] -'
+  )
+  if [[ -z "${version}" ]]; then
     version=$(
-        gh api \
-            --method GET \
-            --header 'Accept: application/vnd.github+json' \
-            --jq '.tag_name' \
-            "https://api.github.com/repos/$repo/releases/latest" 2>/dev/null | grep -v 'Not Found' | tr -d '[a-z][A-Z] -'
+      gh api \
+        --method GET \
+        --header 'Accept: application/vnd.github+json' \
+        --jq '.[0].name' \
+        "https://api.github.com/repos/$repo/tags" | tr -d '[a-z][A-Z] -'
     )
-    if [[ -z "${version}" ]]; then
-        version=$(
-            gh api \
-                --method GET \
-                --header 'Accept: application/vnd.github+json' \
-                --jq '.[0].name' \
-                "https://api.github.com/repos/$repo/tags" | tr -d '[a-z][A-Z] -'
-        )
-    fi
+  fi
 
-    echo "  $name: $version" >>$filename
+  echo "  $name: $version" >>$filename
 
-    # sleep 1s
+  # sleep 1s
 done
