c1dad19f28b0b634c93754f3cfbc3c5180e1c334
Kiba Amor <5093911+kibaamor@users.noreply.github.com>
2025-04-13 23:36:11 +0800

    update powershell profile ps1

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,12 @@
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\markbull.omp.json" | Invoke-Expression
+
+# WinGet (https://github.com/microsoft/winget-cli)
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+    [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+    $Local:word = $wordToComplete.Replace('"', '""')
+    $Local:ast = $commandAst.ToString().Replace('"', '""')
+    winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+      [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+    }
+}
